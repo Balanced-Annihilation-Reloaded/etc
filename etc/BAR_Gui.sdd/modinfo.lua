786ca84dc38098e746c709787e193809d11d6fa0
koshi <koshi@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-06-12 21:58:02 +0000

    adds framework/dummy files for sasi

---
+++ etc/BAR_Gui.sdd/modinfo.lua
@@ -0,0 +1,12 @@
+return {
+  name='BAR_Gui',
+  description='BAR',
+  shortname='BAR_Gui',
+  version='VERSION',
+  mutator='Official',
+  game='BAR_Gui',
+  shortGame='BAR_Gui',
+  modtype='T',
+  depend = { },
+}
+
---
+++ etc/BAR_Gui.sdd/qml/views/DText.qml
@@ -0,0 +1,6 @@
+import QtQuick 1.0
+
+Text {
+	FontLoader { id: localFont; source: "../fonts/Ubuntu-R.ttf" }
+	font { family: localFont.name; }
+}

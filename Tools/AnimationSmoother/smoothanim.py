5595baa27bb69d21e66456e6269470998c9f817f
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-06-12 13:35:00 +0000

    ETC: Animation smoother update

--- Tools/AnimationSmoother/smoothanim.py
+++ Tools/AnimationSmoother/smoothanim.py
@@ -1,9 +1,9 @@
 import sys
 import os
-print 'usage: smoothanim.py [input.bos] [minsleep] [maxsleep] [output.bos]'
-minsleep=30
-maxsleep=300
-
+print 'usage: smoothanim.py [input.bos] [minsleep] [maxsleep] [output.bos] [animspeed] [smooth]'
+minsleep=33
+maxsleep=400
+animspeed=65
 class Piece:
 	rx=0
 	ry=0
@@ -19,10 +19,12 @@ if len(sys.argv)>=3:
 	minsleep=int(sys.argv[2])
 if len(sys.argv)>=4:
 	minsleep=int(sys.argv[3])
+if len(sys.argv)>=6:
+	animspeed=int(sys.argv[5])
 if len(sys.argv)>=5:
 	output=sys.argv[4]
 else:
-	output=sys.arv[1].partition('.')[0]+'_smoothanim.bos'
+	output=sys.argv[1]#.partition('.')[0]+'_smoothanim.bos'
 pieces={}
 #process the bos file
 wholefile=''.join([line.strip().partition('//')[0] for line in inputfile])
@@ -32,7 +34,7 @@ for p in piecenames:
 	if p!='':
 		pieces[p]={'move':{'x':0,'y':0,'z':0},'turn':{'x':0,'y':0,'z':0}}
 print pieces
-def parsebos(line): 
+def parsebos(line,verbose=True): 
 	#parses a move or turn command, returns false on any anomaly, 
 	#else it returns a dict: {'c':command,'p':piece,'a':axis,'p':pos,'s':speed,'a':acc}
 	l=line
@@ -41,45 +43,55 @@ def parsebos(line):
 	
 	#move rleg to y-axis [0.250000] now;
 	if len(line)<5:
-		print 'line split is less than 5, not a proper move/turn command!', line,l
+		if verbose:
+			print 'line split is less than 5, not a proper move/turn command!', line,l
 		return False
 	if line[0]!='turn' and line[0]!='move':
-		print 'line[0] is not turn or move',l
+		if verbose:
+			print 'line[0] is not turn or move',l
 		return False
 	else:
 		command=line[0]
 	
 	if line[1] not in pieces:
-		print 'invalid piece',l,'not in piecelist',pieces
+		if verbose:
+			print 'invalid piece',l,'not in piecelist',pieces
 		return False
 	else:
 		piece=line[1]
 	
 	if line[2] != 'to':
-		print "line[2] != to",l
+		if verbose:
+			print "line[2] != to",l
 		return False
 	
 	if '-axis' not in line[3] and not ('x-' in line[3] or 'y-' in line[3] or'z-' in line[3]):
-		print 'bad axis in line[3]',l
+		if verbose:
+			print 'bad axis in line[3]',l
 		return False
 	else:
 		axis=line[3][0]
-		print axis
+		if verbose:
+			print axis
 		if axis not in 'xyz':
-			print 'axis fail!', l,line[3],line[3][0]
+			if verbose:
+				print 'axis fail!', l,line[3],line[3][0]
 			return False
 	
 	try:
 		if command == 'move' and ('[' not in line[4] or ']' not in line[4]):
-			print 'missing [ or ] in pos of move command',l
+			if verbose:
+				print 'missing [ or ] in pos of move command',l
 			return False
 		elif command == 'turn' and ('<' not in line[4] or '>' not in line[4]):
-			print 'missing < or > in pos of turn command',l
+			if verbose:
+				print 'missing < or > in pos of turn command',l
 			return False
 		else:
 			pos=float(line[4].strip('[]<>'))
 	except ValueError:
-		print 'cant parse pos in',l,line
+		if verbose:
+			print 'cant parse pos in',l,line
 		return False
 	
 	if line[5] == 'now':
@@ -87,31 +99,38 @@ def parsebos(line):
 	elif line[5] == 'speed':
 		try:
 			if command == 'move' and ('[' not in line[6] or ']' not in line[6]):
-				print 'missing [ or ] in speed of move command',l
+				if verbose:
+					print 'missing [ or ] in speed of move command',l
 				return False
 			elif command == 'turn' and ('<' not in line[6] or '>' not in line[6]):
-				print 'missing < or > in speed of turn command',l
+				if verbose:
+					print 'missing < or > in speed of turn command',l
 				return False
 			else:
 				speed=float(line[6].strip('[]<>'))
 		except:
-			print 'cant parse speed in',l,line
+			if verbose:
+				print 'cant parse speed in',l,line
 			return False
 	else:
-		print 'bad line[6]',line[6],l
+		if verbose:
+			print 'bad line[6]',line[6],l
 		return False
 	if len(line)>=9 and line[7]=='accelerate':
 		try:
 			if command == 'move' and ('[' not in line[8] or ']' not in line[8]):
-				print 'missing [ or ] in speed of move command',l
+				if verbose:
+					print 'missing [ or ] in speed of move command',l
 				return False
 			elif command == 'turn' and ('<' not in line[8] or '>' not in line[8]):
-				print 'missing < or > in speed of turn command',l
+				if verbose:
+					print 'missing < or > in speed of turn command',l
 				return False
 			else:
 				acc=float(line[8].strip('[]<>'))
 		except:
-			print 'cant parse accelerate in',l,line[8]
+			if verbose:
+				print 'cant parse accelerate in',l,line[8]
 			return False
 	else:
 		acc=0
@@ -121,32 +140,67 @@ for line in inputfile:
 	i+=1
 	if ('move' in line or 'turn' in line) and 'now' in line: # search ahead and find a sleep before a bracket-close
 		sleep=-1
-		for k in range(i,len(inputfile)):
-			if 'sleep' in inputfile[k].partition('//')[0]: # .partition('//')[0] is the remove comment operator :D
-				try:
-					sleep=float(inputfile[k].partition('sleep')[2].partition(';')[0])
+		for k in range(i+1,len(inputfile)):
+			#if there is another move-now after it, then we ignore that.
+			if parsebos(line,False) and parsebos(inputfile[k],False):
+				orig=parsebos(line,False)
+				next=parsebos(inputfile[k],False)
+				if orig['c']==next['c'] and orig['p']==next['p'] and orig['a']==next['a']:
+					print 'There is an identical move order ',k-i,'lines after, we just need to update the piece pos!'
+					pieces[orig['p']][orig['c']][orig['a']]=orig['pos']
 					break
-				except ValueError:
-					print 'failed to parse sleep in line',inputfile[k],'skipping'
-					continue
+			if 'sleep' in inputfile[k].partition('//')[0]: # .partition('//')[0] is the remove comment operator :D
+				if 'animSpeed' in inputfile[k].partition('//')[0]:
+					sleep='animSpeed'
+					break
+				else:
+					if 'currentSpeed' in inputfile[k].partition('//')[0]:
+						sleep='currentSpeed'
+						try:
+							animspeed=int(inputfile[k].partition('sleep')[2].partition('/')[0])/100
+							break
+						except ValueError:
+							print 'failed to parse sleep in line',inputfile[k],'skipping'
+							continue
+
+					else:
+						try:
+							sleep=float(inputfile[k].partition('sleep')[2].partition(';')[0])
+							break
+						except ValueError:
+							print 'failed to parse sleep in line',inputfile[k],'skipping'
+							continue
 			if '}' in inputfile[k].partition('//')[0]:
 				print 'no sleep after now'
 				break
-		if sleep>0: # we have the sleep value, time to find the last position the piece was at before this new NOW command!
+		if sleep=='animSpeed' or sleep=='currentSpeed' or (sleep>minsleep and sleep < maxsleep): # we have the sleep value, time to find the last position the piece was at before this new NOW command!
+			#important thing about sleep: #of frames= CEILING(sleep/33) (which means sleep 33 sleeps 2 frames!)
 			if parsebos(line):
 				bos=parsebos(line)
 				print bos
 				oldpos=pieces[bos['p']][bos['c']][bos['a']]
 				if bos['s']=='now':
 					dist=abs(oldpos-bos['pos'])
-					speed=dist/(sleep/1000)
+					if sleep=='animSpeed':
+						smanim=True
+						sleep=animspeed
+					else:
+						smanim=False
+					if sleep=='currentSpeed':
+						sleep=animspeed
+					sleep=(int(sleep)/33+1)*33
+					print sleep
+					speed=dist/(float(sleep)/990)
 					if dist!=0:
 						if bos['c']=='turn':
-							s='speed <%f>'
+							
+							s='speed <%f>'%(speed)
 						else:
-							s='speed [%f]'
-						inputfile[i]=line.replace('now',s%(speed))
-					
+							s='speed [%f]'%(speed)
+						if smanim:
+							s+=' * 100 / currentSpeed'
+						inputfile[i]=line.replace('now',s)
+				
 					
 				pieces[bos['p']][bos['c']][bos['a']]=bos['pos']
 			else:

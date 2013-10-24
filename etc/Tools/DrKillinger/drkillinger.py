9e7a5936ff12dca0eadee9d5a69e41c8ba13ea5b
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-10-24 22:35:55 +0000

    ETC: dr killinger checkpoint

--- etc/Tools/DrKillinger/drkillinger.py
+++ etc/Tools/DrKillinger/drkillinger.py
@@ -3,22 +3,68 @@ import os
 from s3o import S3O
 
 from Tkinter import *
+# from ttk import *
 import tkFileDialog
 import tkFont
 # severities: wreck, heap, destroy
 
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+
+    """
+    def __init__(self, parent, *args, **kw):
+        Frame.__init__(self, parent, *args, **kw)            
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
+
 class App:
 
 	def __init__(self, master):
+		
 		self.initialdir=os.getcwd()
 		master.title('Dr. Killinger - By Beherith - Thanks to Muon\'s wonderful s3o library!')
 		self.frame = Frame(master)
 		self.topframe = Frame(self.frame, bd=1, relief = SUNKEN)
-		self.severitiesframe = Frame(self.frame, bd=3, relief = SUNKEN)
-		self.frame.pack()
+		self.severitiesframe = VerticalScrolledFrame(self.frame)
+		self.frame.pack(fill=Y)
 		self.topframe.pack(side=TOP,fill=X)
 		self.severitiesframe.pack(side=TOP,fill=X)
-		
+		self.validflags=['SHATTER','EXPLODE','FALL','SMOKE','FIRE','NONE','NO_CEG_TRAIL','NO_HEATCLOUD']
 		self.menuframe=Frame(self.topframe,bd=3,relief=SUNKEN)
 		self.treeframe=Frame(self.topframe,bd=2,relief=SUNKEN)
 		self.menuframe.pack(side=LEFT,fill=Y)
@@ -37,27 +83,31 @@ class App:
 		##========================
 		
 		##TREEFRAME:
-		treefont=tkFont.Font(family='Courier New',size=10)
+		self.treefont=tkFont.Font(family='Courier New',size=10)
 		self.treelabeltext=StringVar()
 		
 		self.treelabeltext.set('Tree goes here')
-		self.treelabel=Label(self.treeframe, textvariable=self.treelabeltext, justify=LEFT, font=treefont)
+		self.treelabel=Label(self.treeframe, textvariable=self.treelabeltext, justify=LEFT, font=self.treefont)
 		self.treelabel.pack(side=LEFT)
 		
 		##============================
 		
 		##==severityframe;
 		self.wreckframe=Frame(self.severitiesframe,bd=3,relief=SUNKEN)
-		self.wreckframe.pack(side=LEFT,fill=X)
+		self.wreckframe.pack(side=TOP,fill=X)
 		Label(self.wreckframe, text='Wreck').pack(side=LEFT)		
 		
 		self.heapframe=Frame(self.severitiesframe,bd=3,relief=SUNKEN)
-		self.heapframe.pack(side=LEFT,fill=X)
+		self.heapframe.pack(side=TOP,fill=X)
 		Label(self.heapframe, text='Heap').pack(side=LEFT)		
 		
 		self.destroyframe=Frame(self.severitiesframe,bd=3,relief=SUNKEN)
-		self.destroyframe.pack(side=LEFT,fill=Y)
+		self.destroyframe.pack(side=TOP,fill=X)
 		Label(self.destroyframe, text='Destroy').pack(side=LEFT)
+		
+		self.annihilateframe=Frame(self.severitiesframe,bd=3,relief=SUNKEN)
+		self.annihilateframe.pack(side=TOP,fill=X)
+		Label(self.annihilateframe, text='annihilate').pack(side=LEFT)
 		#======
 		
 		#==== common objects:
@@ -71,6 +121,8 @@ class App:
 		self.unitdef=0
 		self.piecelist=[]
 		self.killscript={}
+		self.uiframes=[self.wreckframe,self.heapframe,self.destroyframe,self.annihilateframe]
+		self.severitylevels=[25,50,99,-1]
 		#=====
 	def loadmod(self):
 		return
@@ -91,14 +143,53 @@ class App:
 			print 'loaded',self.unitname,'successfully'
 			self.updatetree(self.s3o,self.piecelist)
 			print self.piecelist
-			self.loadbos(self.bos)
-			
+			self.killscript=self.loadbos(self.bos)
+			print self.killscript
+			self.createui(self.killscript)
 		return
 	def updatetree(self,model,pl):
 		namejust=20
-		treestr='NAME                     vol #tris ox  oy  oz\n'+recursepiecetree(model.root_piece,0,(0,0,0),pl)
-		
+		treestr='NAME                        volume (elmos) #tris  ox   oy   oz\n'+recursepiecetree(model.root_piece,0,(0,0,0),pl)
 		self.treelabeltext.set(treestr)
+	def createui(self,killtable):
+		
+		i=0
+		for ctype in killtable:
+			
+			sevframe=Frame(self.uiframes[i])
+			sevframe.pack(side=TOP,fill=X)
+			sev=self.severitylevels[i]
+			if 'severity' in ctype:
+				sev=ctype['severity']
+			ctype['severity']=StringVar()
+			ctype['severity'].set(str(sev))
+			
+			Label(sevframe, text='Severity <=').pack(side=LEFT)
+			Entry(sevframe,width=4,textvariable=ctype['severity']).pack(side=LEFT)
+			
+			for piece in self.piecelist:
+				if piece in ctype:
+					self.makerow(i,piece,ctype[piece])
+				else:
+					self.makerow(i,piece,[])
+			i+=1
+	def makerow(self, sevlevel, piece, flags):
+		print sevlevel
+		rowframe=Frame(self.uiframes[sevlevel])
+		rowframe.pack(side=TOP,fill=X)
+		Label(rowframe,text=piece.ljust(12),font=self.treefont).pack(side=LEFT)
+		
+		self.killscript[sevlevel][piece]={}
+	
+		for flag in self.validflags:
+			val=IntVar()
+			val.set(0)
+			if flag in flags:
+				val.set(1)
+				print flag,piece,'should be set'
+			self.killscript[sevlevel][piece][flag]=val
+			Checkbutton(rowframe,text=flag, variable=val).pack(side=LEFT)
+		
 	def loadbos(self, boslines):
 		bospiecelist=[]
 		killtable=[{},{},{},{}]
@@ -108,7 +199,7 @@ class App:
 
 			#strip comments:
 			line=uncomment(line)
-			if line[0:5]=='piece':
+			if line[0:5]=='piece' and bospiecelist==[]:
 	
 				if ';' not in line:
 					line+=uncomment(boslines[l+1])+uncomment(boslines[l+2])+uncomment(boslines[l+3]) #wow this is ugly, i cant write parsers for shit.
@@ -127,12 +218,19 @@ class App:
 						p=delimit(line,'explode','type')
 						flags=[x.strip() for x in delimit(line,'type',';').split('|')]
 						print p,killedindex, flags
-						killtable[killedindex][p]=flags
+						if 'EXPLODE_ON_HIT' in flags and 'EXPLODE' not in flags:
+							del flags[flags.index('EXPLODE_ON_HIT')]
+							flags.append('EXPLODE')
+						for flag in flags:
+							if flag not in self.validflags:
+								del flags[flags.index(flag)]
+								
+						killtable[killedindex][p.lower()]=flags
 						
 				break
 
 			l+=1	
-		if len(bospiecelist)!=self.piecelist:
+		if len(bospiecelist)!=len(self.piecelist):
 			print 'WARNING: the bos piece list does not match the s3o piece list!', bospiecelist, self.piecelist
 			
 		return killtable
@@ -167,7 +265,7 @@ def recursepiecetree(piece, depth,offset,pl): # we need the name offset by depth
 	namejust=20
 	s='%s %10.1f %5i %4.2f %4.2f %4.2f\n'%(('  '*depth+piece.name).ljust(30),piecevolume(piece), len(piece.indices), piece.parent_offset[0]+offset[0], piece.parent_offset[1]+offset[1], piece.parent_offset[2]+offset[2])
 	print s
-	pl.append(piece.name)
+	pl.append(piece.name.lower())
 	for child in piece.children:
 		s+=recursepiecetree(child, depth+1, (piece.parent_offset[0]+offset[0], piece.parent_offset[1]+offset[1], piece.parent_offset[2]+offset[2]),pl)
 	return s
@@ -184,14 +282,14 @@ def piecevolume(piece):
 root = Tk()
 app = App(root)
 root.mainloop()
-validflags=['SHATTER','EXPLODE','EXPLODE_ON_HIT','FALL','SMOKE','FIRE','NONE','NO_CEG_TRAIL','NO_HEATCLOUD']
+validflags=['SHATTER','EXPLODE','FALL','SMOKE','FIRE','NONE','NO_CEG_TRAIL','NO_HEATCLOUD']
 #flagrules:
 # EXPLODE=EXPLODE_ON_HIT  
 # EXPLODE causes it to do 50 damage on impact!
 #all pieces bounce with p=0.66
 #void CUnitScript::Explode(int piece, int flags)
 #flag processing order
-#1. noheatcloud = obvious no heatcloud at site of explosion
+#1. noheatcloud = obvious no heatcloud at site of explosion, heatcloudtex is bitmaps/explo.tga
 #2. NONE: no stuff falls off, return
 #3. SHATTER: shatters, return
 #4. !! at this point, FALL IS TURNED ON!

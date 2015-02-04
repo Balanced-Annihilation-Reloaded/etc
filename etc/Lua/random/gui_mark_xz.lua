68502822a40198c5dffbebeb70da1ffe5ce70d55
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-02-04 15:28:23 +0000

    add widget to mark map x,z of current mouse location into etc/lua

---
+++ etc/Lua/random/gui_mark_xz.lua
@@ -0,0 +1,26 @@
+function widget:GetInfo()
+    return {
+        name      = "Mark XZ",
+        version   = "0",
+        desc      = "Press m to place a marker on the map at current mouse pos with x,z coords",
+        author    = "Pope Brian IX",
+        date      = "2000 BC",
+        license   = "Je suis une pamplemousse",
+        layer     = 0,
+        enabled   = true
+	}
+end 
+
+include('keysym.h.lua')
+
+function widget:KeyPress(key,_,_)
+    if key==KEYSYMS.M then
+        local mx,my,_,_,_ = Spring.GetMouseState()
+        local t,l = Spring.TraceScreenRay(mx,my)
+        if t=="ground" then
+            Spring.MarkerAddPoint(l[1],l[2],l[3],tostring(math.floor(l[1]))..", "..tostring(math.floor(l[3])))
+        end
+    end
+end
+
+

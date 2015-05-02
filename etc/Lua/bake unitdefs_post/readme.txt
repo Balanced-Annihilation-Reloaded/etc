646904892d19879c0f1a808679a1f2acc65827dc
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-05-02 08:12:42 +0000

    add functionality to bake unitdefs_post into unitdefs to etc/lua

---
+++ etc/Lua/bake unitdefs_post/readme.txt
@@ -0,0 +1,12 @@
+Funtionality to bake unitdefs_post into unitdef files
+
+(1) Put unitdefs_post_save_to_customparams.lua in gamedata
+(2) Put 
+(3) Add VFS.Include("gamedata/unitdefs_post_save_to_customparams.lua")
+ to unitdefs_post, at the point where you want to bake
+(4) Load Spring, then load the widget
+(5) Look in the spring folder, unitdefs should all be written to file
+
+Notes:
+! Make sure your unit defs don't contain lua keywords as table keys remove them with unitdefs_post)
+! Make sure you don't bake in any modoptions that altered unitdefs
---
+++ etc/Lua/bake unitdefs_post/unitdefs_write.lua
@@ -0,0 +1,37 @@
+function widget:GetInfo()
+	return {
+		name      = "Write ud.customparam.__ud to files",
+		desc      = "Bluestone",
+		author    = "Robert De Bruce",
+		date      = "-1",
+		license   = "Those stupid trees",
+		layer     = 0,
+		enabled   = false  --  loaded by default?
+	}
+end
+
+
+function widget:Initialize()
+    local had_failed = false
+    for k,v in pairs(UnitDefs) do
+        if not v.customParams or not v.customParams.__ud then
+            Spring.Echo("Could not find ud.customparams.__ud, check that you ran unitdefs_post_save_to_customparams")
+        end
+        local ud_string = v.customParams.__ud --from table.tostring in unitdefs_post 
+        ud_string = "return { " .. v.name .. " = " .. ud_string .. "}" 
+        local f = loadstring(ud_string)
+        if f then
+            local ud_table = f()
+            table.save(ud_table, v.name .. ".lua","")
+        else
+            had_failed = true
+            Spring.Echo("FAILED: " .. v.name, ud_string)
+        end
+    end
+    if had_failed then
+        Spring.Echo("Some ud_string failed to convert to table. Maybe check that your table keys do not contain lua keywords?")
+    else
+        Spring.Echo("Wrote all ud_string to files")
+    end
+    widgetHandler:RemoveWidget(self)
+end

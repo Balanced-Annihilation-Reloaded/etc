ea99e0718ae169bb8bb69196bb16942d265a07a9
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-05-23 06:22:00 +0000

    remove empty customparams tables

--- Lua/bake unitdefs_post/unitdefs_write.lua
+++ Lua/bake unitdefs_post/unitdefs_write.lua
@@ -26,8 +26,13 @@ function widget:Initialize()
         local f = loadstring(ud_string)
         if f then
             local ud_table = f()
-            for k,_ in pairs(ud_table) do
-                if #ud_table[k].customparams==0 then ud_table[k].customparams=nil end
+            for k,_ in pairs(ud_table) do -- remove the customParams table if it is empty
+                local isEmpty = true
+                for k2,_ in pairs(ud_table[k].customparams) do
+                    isEmpty = false
+                    break
+                end
+                if isEmpty then ud_table[k].customparams=nil end
             end
             table.save2(ud_table, v.name .. ".lua")
         else
